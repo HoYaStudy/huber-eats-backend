1bd91b77873ca470713eb4fade2a52bc9544c03a
llHoYall <hoya128@gmail.com>
2021-01-13 21:29:39 +0900

    Added Chapter #3: TypeORM and Nest

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,29 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@InputType({ isAbstract: true }) // for DTO
+@ObjectType() // for GraphQL
+@Entity() // for TypeORM
 export class Restaurant {
-  @Field(type => String)
+  @Field(type => Number) // for GraphQL
+  @PrimaryGeneratedColumn() // for TypeORM
+  id: number;
+
+  @Field(type => String) // for GraphQL
+  @Column() // for TypeORM
+  @IsString()
+  @Length(5)
   name: string;
 
-  @Field(type => Boolean)
+  @Field(type => Boolean, { nullable: true }) // for GraphQL
+  @Column({ default: false }) // for TypeORM
+  @IsOptional()
+  @IsBoolean()
   isVegan?: boolean;
 
-  @Field(type => String)
+  @Field(type => String, { defaultValue: 'KangNam' }) // for GraphQL
+  @Column() // for TypeORM
+  @IsString()
   address: string;
-
-  @Field(type => String)
-  ownersName: string;
 }
